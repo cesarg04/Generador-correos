e7e3dbff1c743777e2fcb3a64b8deddcef8fb9b1
Cesar Garcia <elduro2002_@hotmail.com>
2021-12-04 11:42:59 -0400

    Estrapulador de correos V2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
 const inputEmail = document.getElementById('entrada-correo');
 const selectEmail = document.querySelector("#select-mail").value;
 const enterButon = document.querySelector("#enter-button");
+const clearButton = document.querySelector("#clear-button");
 const showInfo = document.querySelector("#show-info");
 
 // Selected the provider Email
@@ -21,52 +21,95 @@ const showNumber = () => {
 
 //Fuction to generate Aliatorie numbers
 const numeroAleatorio = (maximo, minimo) => {
-    return Math.floor(Math.random() * ((maximo+1)-minimo)+minimo);
+    return Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
 }
 
 
 const iterationEmails = (data) => {
-    let element = document.createElement(`p`)
-    element.innerHTML= `${data}`;
+    let element = document.createElement(`p`);
+    element.className = 'lista-email';
+    element.innerHTML = `${data}`;
     showInfo.append(element);
 }
 
+//Test input
+/*
+const testInput = () => {
+    if (inputEmail.indexOf("") <= -1) {
+        console.log('El input esta vacio');
+        
+    }
+}
+*/
 
 //Event click
 const runProgram = () => {
-
     let convertEmail = inputEmail.value.split("");
-    console.log(numeroAleatorio(convertEmail.length, 1))
-        
-    let resultadoAleatorio = numeroAleatorio(convertEmail.length, 1);
-    let converted = convertEmail.splice(resultadoAleatorio, 0, ".");
-    let stringc = convertEmail.join('');
 
-    const unin = () => stringc+ShowSelected();
-    const exportArray = () => convertEmail;
+    let resultadoAleatorio = numeroAleatorio(convertEmail.length, 1);
+
+    // ReRun Dop
+    let usadosTest = new Array;
+    usadosTest.push(resultadoAleatorio)
+
+    let converted = convertEmail.splice(resultadoAleatorio, 0, ".");
+
+    for (const i in usadosTest) {
+
+        if (resultadoAleatorio === usadosTest[i]) {
+            let arr = []
+            arr.push(usadosTest[i]);
+            let arr2 = [];
+            arr2.push(resultadoAleatorio)
+            console.log(resultadoAleatorio)
+
+            let newResultado = numeroAleatorio(convertEmail.length, 1);
+            convertEmail.splice(newResultado, 0, ".");
+        };
+        //console.log(usadosTest);
+    };
+
+
+    let stringc = convertEmail.join('');
+    const unin = () => stringc + ShowSelected();
 
     iterationEmails(unin())
+
 }
 
+// Test of parent the emails
+
 const reRunProgram = () => {
-    
+
     for (let index = 0; index < showNumber(); index++) {
         runProgram()
     }
-    
 }
 
 enterButon.addEventListener('click', () => {
-    reRunProgram()  
+    reRunProgram()
 })
 
-inputEmail.addEventListener('keyup', ({key}) => {
+// Fuction to clear screen of emails
+
+const clearEmail = () => {
+    const listas = document.querySelector(".lista-email");
+    showInfo.removeChild(listas);
+}
+
+const reClear = () => {
+    for (let init = 0; init < showNumber(); init++) {
+        clearEmail()
+    }
+}
+
+inputEmail.addEventListener('keyup', ({ key }) => {
     if (key === "Enter") {
         reRunProgram()
     }
 })
 
-
-
-
+clearButton.addEventListener('click', () => {
+    reClear();
+});
 
